aa07c9e6f3deecb54fb5ad300e8c55b4434d7308
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-01-31 14:56:44 -0300

    Mapeamento da Classe Sindico

--- src/main/java/com/ramon/sindicon/model/Sindico.java
+++ src/main/java/com/ramon/sindicon/model/Sindico.java
@@ -1,8 +1,20 @@
 package com.ramon.sindicon.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
 public class Sindico {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String nomeEdificio;
     private String telefone;
@@ -11,5 +23,4 @@ public class Sindico {
     private String numero;
     private String bairro;
     private String cidade;
-    
 }
